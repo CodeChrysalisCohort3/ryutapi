c0905d5ee94153b12c0a418d3be187a2d207217a
有働龍太 <RYUTA@udouryuuta-no-MacBook-Air-2.local>
2018-01-27 12:27:51 +0900

    create index.html

--- index.js
+++ index.js
@@ -28,7 +28,7 @@ app.use((req, res, next) => {
 app.use(bodyParser.json({ type: 'application/json', limit: '50mb' }));
 
 app.use('/api', apiRouter);
-app.use(express.static(`${__dirname}/public`));
+app.use(express.static(`${__dirname}/client`));
 
 app.use((err, req, res, next) => {
   if (err.stack) {
