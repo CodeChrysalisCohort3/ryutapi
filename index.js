8b72612d4b37d0dce20d743d4b6f79b131edcbd9
有働龍太 <RYUTA@udouryuuta-no-MacBook-Air-2.local>
2018-01-27 11:43:20 +0900

    create services/index.js

---
+++ index.js
@@ -0,0 +1,2 @@
+const config = require('./config');
+const services = require('./services')(config);
---
+++ services/index.js
@@ -0,0 +1,13 @@
+const Promise = require('bluebird');
+const fs = Promise.promisifyAll(require('fs'));
+
+module.exports = (config) => {
+  let services = {};
+  fs.readdirSync(__dirname).forEach(fileName => {
+    if(fileName.indexOf('.') === -1) {
+      const serviceName = fileName;
+      services[fileName] = require(`${__dirname}/${fileName}`)(config[fileName]);
+    }
+  });
+  return services;
+};
